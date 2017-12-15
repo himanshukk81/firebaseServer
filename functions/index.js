3b5d6f857c0d3e3552cff19f763d196293a30510
himanshu <himanshukk81@gmail.com>
2017-12-15 18:12:36 +0530

    changes

--- functions/index.js
+++ functions/index.js
@@ -64,7 +64,7 @@ exports.fcmSendUser = functions.database.ref('/user_detail/{userId}').onCreate(e
 	      click_action: "https://autorepair-1510293342488.firebaseapp.com"
 	    }
 	};
-	admin.database()
+		admin.database()
 	   .ref(`/fcmTokens`)
 	   .on("value", function(snapshot){
 	 		var counter=0;
@@ -176,5 +176,143 @@ exports.fcmSendMessage = functions.database.ref('/messages/{messageId}').onCreat
 		}, function (errorObject) {
 		  console.log("The read failed: " + errorObject.code);
 		});
+
+});
+
+
+exports.fcmSendMessage = functions.database.ref('/orders/{id}').onCreate(event => {
+  console.log("message sent============");
+  var data=event.data.val();
+  console.log("Data======"+JSON.stringify(data));
+  
+
+
+  	if(data.status=='P')
+  	{
+			const payload = {
+		    notification: {
+		      title:"Hellooo",
+		      icon: "https://placeimg.com/250/250/people",
+		      click_action: "https://autorepair-1510293342488.firebaseapp.com"
+		    }
+		};
+		admin.database()
+	   .ref(`/fcmTokens`)
+	   .on("value", function(snapshot){
+	 		var counter=0;
+	    	var token=snapshot.val();
+	    	console.log("token===="+token);
+	    	var userTokens=[];
+	    	for(const prop in token)
+	    	{
+	    		// console.log("data==="+token[prop]);
+	    		userTokens.push(token[prop]);
+	    	}
+	    	function checkUserToken()
+			{
+				if(userTokens.length>0)
+				{
+				    if(counter==userTokens.length)
+	        		{
+
+	        			userTokens=[];
+	        		}
+	        		else
+	        		{
+	        			notificationToAdmin(userTokens[counter]);
+
+	        		}		
+				}
+					
+			}
+	    		
+	    		
+	    	checkUserToken();
+
+
+	    	function notificationToAdmin(token)
+	    	{
+	    		admin.messaging().sendToDevice(token, payload).then((res)=>{
+	    			console.log("Successfully got==="+JSON.stringify(res));
+	    			counter++;
+	    			checkUserToken();
+	    			
+	    		},error=>{
+	    			console.log("Failed");
+	    		})	
+	    	}
+
+
+	    	// console.log("key data==="+snapshot.key)
+	  		// console.log(snapshot.val());
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
+		});	
+  	}
+  	else
+  	{
+  		admin.database()
+	   .ref(`/user_detail/${data.userId}`)
+	   .on("value", function(snapshot) {
+	   		var user=snapshot.val();
+	   		console.log("User info===="+JSON.stringify(user));
+	   		if(user.deviceToken &&  user.login)
+	   		{
+	   			notificationToUser(user.deviceToken)
+	   		}
+	   		else
+	   		{
+	   			console.log("No Active Device token for user");
+	   		}
+
+
+	   			function notificationToUser(token)
+	    	{
+
+	    		// console.log("user token145===="+token);
+	    		var message;
+	    		if(data.status=='A')
+	    		{
+	    			message="Approved";
+	    		}
+	    		else
+	    		{
+	    			message="Reject";
+	    		}
+	    		const payload = {
+				    "notification": {
+				      "title":"Order notification",
+				      "body":"Your order is"+message,
+				      "sound":"default",
+				      "click_action": "FCM_PLUGIN_ACTIVITY",
+				      "icon": "https://placeimg.com/250/250/people",
+				    },
+				    "data":
+				        {
+				        	"receiverId":data.receiverId,
+					        "message":data.editorMsg,
+					        "content-available": '1' //FOR CALLING ON NOTIFACATION FUNCTION EVEN IF THE APP IS IN BACKGROUND
+					    },
+				};
+
+				// console.log("Pay load info==="+JSON.stringify(pay))
+	    		admin.messaging().sendToDevice(token, payload).then((res)=>{
+	    			console.log("Successfully notify user"+JSON.stringify(res));
+	    		},error=>{
+	    			console.log("Failed=="+error);
+	    		})	
+
+
+
+
+
+
+	    	}
+
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
+		});
+  	}
+  	
 
 });
